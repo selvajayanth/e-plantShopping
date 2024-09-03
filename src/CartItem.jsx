e401c15f001cbad33603537adc001588a434881b
selvajayanth <selvakumarjayanth@gmail.com>
2024-09-03 08:58:12 -0400

    E-plants project commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,43 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    for(let items of cart){
+      totalAmount += calculateTotalCost(items);
+    }
+    return totalAmount;
   };
 
-  const handleContinueShopping = (e) => {
-   
+  const handleContinueShopping = () => {
+   if(onContinueShopping){
+    onContinueShopping();
+   }
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name:item.name,quantity:item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+   if(item.quantity>1){
+    dispatch(updateQuantity({name:item.name,quantity:item.quantity-1}));
+   }else{
+    dispatch(removeItem(item.name));
+   }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const cost = parseFloat(item.cost.replace('$', ''));
+    return cost*item.quantity;
   };
 
   return (
@@ -55,9 +71,9 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button className="get-started-button" onClick={(e) => handleContinueShopping()}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
